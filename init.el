5a9bc642b6443d1624fc44f9a5b5223e36856de1
Kevin Beam <kwbeam@gmail.com>
2011-11-23 12:17:43 -0600

    New package.el and fixed up loading packages

--- init.el
+++ init.el
@@ -14,8 +14,6 @@
 (setq dotfiles-dir (file-name-directory
                     (or (buffer-file-name) load-file-name)))
 
-;; Load up ELPA, the package manager
-
 (add-to-list 'load-path dotfiles-dir)
 
 (setq autoload-file (concat dotfiles-dir "loaddefs.el"))
@@ -23,15 +21,12 @@
 (setq custom-file (concat dotfiles-dir "custom.el"))
 
 (require 'package)
-(dolist (source '(("marmalade" . "http://marmalade-repo.org/packages/")
-                  ("elpa" . "http://tromey.com/elpa/")))
-  (add-to-list 'package-archives source t))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 (require 'kwb-elpa)
 
 ;; These should be loaded on startup rather than autoloaded on demand
 ;; since they are likely to be used in every session
-
 (require 'cl)
 (require 'saveplace)
 (require 'ffap)
@@ -40,7 +35,6 @@
 (require 'recentf)
 
 ;; Load up starter kit customizations
-
 (require 'kwb-defuns)
 (require 'kwb-bindings)
 (require 'kwb-misc)
@@ -49,7 +43,6 @@
 
 (regen-autoloads)
 (load custom-file 'noerror)
-
 
 ;; ***************************************************************************
 ;; General Emacs Stuff
@@ -62,6 +55,9 @@
 (set-default-font "Inconsolata")
 (set-face-attribute 'default nil :height 120)
 
+(require 'color-theme)
+(require 'color-theme-zenburn)
+
 ;; tabs are two spaces
 (setq default-tab-width 2)
 (setq tab-width 2)
@@ -72,10 +68,19 @@
 ;; automatically sync up external changes to files
 (global-auto-revert-mode t)
 
+;; ***************************************************************************
+;; Markdown
+;; ***************************************************************************
+(autoload 'markdown-mode "markdown-mode"
+  "Major mode for editing Markdown files" t)
+(setq auto-mode-alist
+      (cons '("\\.md" . markdown-mode) auto-mode-alist))
 
 ;; ***************************************************************************
 ;; Ruby
 ;; ***************************************************************************
+(require 'rvm)
+(rvm-use-default)
 
 ;; add line numbers
 (add-hook 'kwb-code-modes-hook
@@ -83,30 +88,31 @@
 (add-hook 'ruby-mode-hook
           (lambda () (run-hooks 'kwb-code-modes-hook)))
 
-
 ;; ***************************************************************************
 ;; Scheme
 ;; ***************************************************************************
-
 (setq scheme-program-name "mit-scheme")
 
+;; ***************************************************************************
+;; Clojure
+;; ***************************************************************************
+(require 'clojure-mode)
 
 ;; ***************************************************************************
 ;; ERC
 ;; ***************************************************************************
-
 (setq erc-nick "kwbeam")
-
 
 ;; ***************************************************************************
 ;; Slime
 ;; ***************************************************************************
-
+(require 'slime)
 ;; unknown failure in slime unless this is defined
 (setq warning-suppress-types nil)
 
-
-;; You can keep system- or user-specific customizations here
+;; ***************************************************************************
+;; User and System specific customizations
+;; ***************************************************************************
 (setq system-specific-config (concat dotfiles-dir system-name ".el")
       user-specific-config (concat dotfiles-dir user-login-name ".el")
       user-specific-dir (concat dotfiles-dir user-login-name))
@@ -116,5 +122,3 @@
 (if (file-exists-p user-specific-config) (load user-specific-config))
 (if (file-exists-p user-specific-dir)
   (mapc #'load (directory-files user-specific-dir nil ".*el$")))
-
-;;; init.el ends here

48113da1593185331878139ab6ffa3cb32c30157
Kevin Beam <kwbeam@gmail.com>
2011-11-23 21:29:28 -0600

    Removed unused bindings

--- kwb-bindings.el
+++ kwb-bindings.el
@@ -1,9 +1,6 @@
 ;; Window switching
 (windmove-default-keybindings) ;; Shift+direction
 
-;; Completion that uses many different methods to find options.
-(global-set-key (kbd "M-/") 'hippie-expand)
-
 ;; Font size
 (define-key global-map (kbd "C-+") 'text-scale-increase)
 (define-key global-map (kbd "C--") 'text-scale-decrease)
@@ -11,15 +8,9 @@
 ;; Use regex searches by default.
 (global-set-key (kbd "C-s") 'isearch-forward-regexp)
 (global-set-key (kbd "\C-r") 'isearch-backward-regexp)
-(global-set-key (kbd "C-M-s") 'isearch-forward)
-(global-set-key (kbd "C-M-r") 'isearch-backward)
 
 ;; File finding
-(global-set-key (kbd "C-x f") 'recentf-ido-find-file)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
-
-;; Help should search more than just commands
-(global-set-key (kbd "C-h a") 'apropos)
 
 ;; So good!
 (global-set-key (kbd "C-x g") 'magit-status)

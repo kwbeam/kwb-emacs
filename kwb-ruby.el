a2e96040fcce66925c98274d376d8404f9d949f5
Kevin Beam <kwbeam@gmail.com>
2011-11-26 18:15:22 -0600

    Removed vagrant reference

--- kwb-ruby.el
+++ kwb-ruby.el
@@ -18,7 +18,6 @@
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
 
 (add-hook 'ruby-mode-hook 'run-coding-hook)
 

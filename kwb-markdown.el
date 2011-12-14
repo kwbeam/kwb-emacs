a30e1756afa6d71942883774444a13248f9179ce
Kevin Beam <kwbeam@gmail.com>
2011-12-13 20:34:59 -0600

    added comments

--- kwb-markdown.el
+++ kwb-markdown.el
@@ -1,3 +1,4 @@
+;; stuff ending with .md is also markdown
 (add-to-list 'auto-mode-alist '("\\.md" . markdown-mode))
 
 (provide 'kwb-markdown)
--- kwb-scheme.el
+++ kwb-scheme.el
@@ -1,3 +1,4 @@
+;; set the scheme to use
 (setq scheme-program-name "mit-scheme")
 
 (provide 'kwb-scheme)

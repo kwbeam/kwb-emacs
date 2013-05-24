59f42171d1210eb1adec07aa35a82f8df7d7e26b
Kevin Beam <kwbeam@gmail.com>
2013-05-23 18:16:22 -0600

    Eliminate kwb-packages.el and simplify loading

--- init.el
+++ init.el
@@ -1,34 +1,69 @@
+;;; init --- kwbeam emacs configuration
+;;; Commentary:
+
 ;; In the beginning...
 ;;
 ;; "Emacs outshines all other editing software in approximately the
-;; same way that the noonday sun does the stars. It is not just bigger
+;; same way that the noonday sun does the stars.  It is not just bigger
 ;; and brighter; it simply makes everything else vanish."
 ;; -Neal Stephenson, "In the Beginning was the Command Line"
 
-;; add common lisp extension
-(require 'cl)
+;;; Code:
 
-;; add to load path
-(setq dotfiles-dir (file-name-directory
-                    (or (buffer-file-name) load-file-name)))
+(setq dotfiles-dir (file-name-directory (or (buffer-file-name)
+                                            load-file-name)))
 (add-to-list 'load-path dotfiles-dir)
 
-;; setup the ELPA Package manager - now in Emacs 24
 (require 'package)
-(package-initialize)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+(package-refresh-contents)
 
-(require 'kwb-packages)
+(defvar kwb-packages
+  '(auto-complete
+    autopair
+    clojure-mode
+    clojure-test-mode
+    coffee-mode
+    dsvn
+    ein
+    exec-path-from-shell
+    expand-region
+    feature-mode
+    flycheck
+    google-this
+    inf-ruby
+    jedi
+    magit
+    markdown-mode
+    multiple-cursors
+    nose
+    paredit
+    python
+    rinari
+    rspec-mode
+    ruby-mode
+    ruby-electric
+    rvm
+    scss-mode
+    solarized-theme
+    virtualenv
+    yasnippet))
 
-;; set and load custom file
+(mapc #'(lambda (package)
+         (unless (package-installed-p package)
+           (package-install package)))
+      kwb-packages)
+
 (setq custom-file (concat dotfiles-dir "custom.el"))
 (load custom-file 'noerror)
 
-;; load specific customizations
 (require 'kwb-general)
 (require 'kwb-personal)
 (require 'kwb-bindings)
 (require 'kwb-dev)
+
+;;; init.el ends here

8d5a68b3a6d4331b1b208f5484ce460a62b7ec08
Kevin Beam <kwbeam@gmail.com>
2013-09-27 21:53:09 -0600

    Always refresh the package contents

--- init.el
+++ init.el
@@ -18,6 +18,7 @@
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-refresh-contents)
 (package-initialize)
 
 (defvar kwb-packages

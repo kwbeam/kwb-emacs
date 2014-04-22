1ca2e414b7303781b4779042ce08cbb920a91e82
Kevin Beam <kwbeam@gmail.com>
2014-04-22 16:47:02 -0600

    Add the MacOS Markdown editor as the Markdown previewer (Mac-only)

--- kwb-dev.el
+++ kwb-dev.el
@@ -131,6 +131,8 @@
 ;; Markdown
 ;; -----------------
 (add-to-list 'auto-mode-alist '("\\.md" . markdown-mode))
+(cond ((eq system-type 'darwin)
+       (setq markdown-open-command "/Applications/Mou.app/Contents/MacOS/Mou")))
 
 ;; -----------------
 ;; JavaScript

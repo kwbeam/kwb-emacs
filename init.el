f11bf9722cf425f402e706057ab2f885797a3556
Kevin Beam <kwbeam@gmail.com>
2014-07-09 13:23:58 -0600

    Map C-c C-g to magit-status.

--- init.el
+++ init.el
@@ -212,6 +212,7 @@
 (define-key global-map (kbd "M-n") 'newline-next)
 
 (global-set-key (kbd "C-!") 'er/expand-region)
+(global-set-key (kbd "C-c C-g") 'magit-status)
 
 
 ;; -------------------------------------

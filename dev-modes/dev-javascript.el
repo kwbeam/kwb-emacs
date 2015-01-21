a4c508f811198588ebb2cb1e3fcfcbe717fa69d5
Kevin Beam <kwbeam@gmail.com>
2015-01-21 13:29:29 -0700

    Turn off tern autocomplete

--- dev-modes/dev-javascript.el
+++ dev-modes/dev-javascript.el
@@ -20,9 +20,5 @@
 
 ;; Add tern
 (add-hook 'js2-mode-hook (lambda () (tern-mode t)))
-(eval-after-load 'tern
-   '(progn
-      (require 'tern-auto-complete)
-      (tern-ac-setup)))
 
 (provide 'dev-javascript)

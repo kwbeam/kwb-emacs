2e8d02c74573966ece78e53b072170fd7e88791b
Kevin Beam <kbeam@nsidc.org>
2015-09-30 16:12:24 -0600

    Use web-mode for jsx

--- dev-modes/dev-web.el
+++ dev-modes/dev-web.el
@@ -3,6 +3,7 @@
 ;; -----------------
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
@@ -12,5 +13,8 @@
 (add-to-list 'load-path "~/tools/HTML5-YASnippet-bundle")
 ;; some time after yasnippet has been loaded execute this
 (require 'html5-snippets)
+
+;; Use eslint
+(flycheck-add-mode 'javascript-eslint 'web-mode)
 
 (provide 'dev-web)

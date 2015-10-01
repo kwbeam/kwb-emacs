cc289568e8a1373188e1b66142462aa0a27d5c9f
Kevin Beam <kwbeam@gmail.com>
2015-09-30 19:39:50 -0600

    Setup javascript with eslint, react, babel

--- dev-modes/dev-javascript.el
+++ dev-modes/dev-javascript.el
@@ -1,18 +1,29 @@
+;;; dev-javascript --- Setup emacs for JavaScript
+;;; Commentary:
 ;; -----------------
 ;; JavaScript
 ;; -----------------
+;; Prerequisites:
+;;   $ sudo npm install -g babel eslint babel-eslint eslint-plugin-react tern
+;; See:
+;;   http://codewinds.com/blog/2015-04-02-emacs-flycheck-eslint-jsx.html
+;;
+;;; Code:
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-hook 'js2-mode-hook 'run-dev-hook)
-(add-hook 'js2-mode-hook 'skewer-mode)
+;;(add-hook 'js2-mode-hook 'skewer-mode)
 
 (setq-default js2-basic-offset 4)
 
 (require 'js2-refactor)
 (js2r-add-keybindings-with-prefix "C-c C-j")
 
-;; use node.js as a repl
-(setq inferior-js-program-command "node")
+;; use node.js with babel as a repl
+(setq inferior-js-program-command "babel-node")
 (setenv "NODE_NO_READLINE" "1")
+(add-to-list 'comint-preoutput-filter-functions
+             (lambda (output)
+               (replace-regexp-in-string "\033\\[[0-9]+[A-Z]" "" output)))
 
 (add-hook 'js2-mode-hook '(lambda ()
 			    (local-set-key "\C-x\C-e" 'js-send-last-sexp)
@@ -26,3 +37,4 @@
 (add-hook 'js2-mode-hook (lambda () (tern-mode t)))
 
 (provide 'dev-javascript)
+;;; dev-javascript.el ends here

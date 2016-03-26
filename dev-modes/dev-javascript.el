a1fcb14268dbc2202ce5c0fb427d3927451af2b0
Kevin Beam <kwbeam@gmail.com>
2016-03-25 21:33:55 -0600

    Add nodejs repl keybindings

--- dev-modes/dev-javascript.el
+++ dev-modes/dev-javascript.el
@@ -18,8 +18,13 @@
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-hook 'js2-mode-hook 'run-dev-hook)
-
 (setq-default js2-basic-offset 4)
+(add-hook 'js2-mode-hook '(lambda ()
+                            (local-set-key "\C-x\C-e" 'nodejs-repl-send-last-sexp)
+                            (local-set-key "\C-cr" 'nodejs-repl-send-region)
+                            (local-set-key "\C-cb" 'nodejs-repl-send-buffer)
+                            (local-set-key "\C-cl" 'nodejs-repl-load-file)
+                            (local-set-key "\C-c\C-z" 'nodejs-repl-switch-to-repl)))
 
 (provide 'dev-javascript)
 ;;; dev-javascript.el ends here

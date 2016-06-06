b1e1c10f1394a02bcceacdb1f666909af8c3bf5c
Kevin Beam <kbeam@nsidc.org>
2016-06-06 08:13:55 -0600

    Add dev mode hook for JSX files (web/tide modes)

--- dev-modes/dev-typescript.el
+++ dev-modes/dev-typescript.el
@@ -23,6 +23,7 @@
 ;; Tide can be used along with web-mode to edit tsx files
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-hook 'web-mode-hook 'run-dev-hook)
 (add-hook 'web-mode-hook
           (lambda ()
             (when (string-equal "tsx" (file-name-extension buffer-file-name))

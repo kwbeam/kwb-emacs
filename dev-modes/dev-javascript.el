602add37c86ed27c2f7016aa2d771a0576bfe0fc
Kevin Beam <kwbeam@gmail.com>
2017-09-26 15:01:04 -0600

    Add lots of JS setup comments for my future self.

--- dev-modes/dev-javascript.el
+++ dev-modes/dev-javascript.el
@@ -3,25 +3,50 @@
 ;; -----------------
 ;; JavaScript
 ;; -----------------
-;; Prerequisites:
-;; ES5:
-;;   * nvm: node.js
-;;   * npm install eslint tern --save-dev
-;; ES6:
-;;   * npm install babel-cli babel-preset-es2015 --save-dev
-;; React:
-;;   * npm install eslint-plugin-react --save-dev
-;;   * npm install babel-preset-react --save-dev
-;;
+;; Prerequisite Emacs packages:
+;;   * add-node-modules-path
+;;   * company
+;;   * company-tern
+;;   * flycheck
+;;   * js2-mode
+;;   * nodejs-repl
+;;   * tern
+;; Prerequisite NPM packages:
+;;   * ES5:
+;;     * nvm
+;;     * NodeJS
+;;     * npm install eslint tern --save-dev
+;;   * ES6:
+;;     * ES5, and
+;;     * npm install babel-cli babel-preset-es2015 --save-dev
+;;   * React:
+;;     * ES5 or ES6, and
+;;     * npm install eslint-plugin-react --save-dev
+;;     * npm install babel-preset-react --save-dev
+;; Notes:
+;;   When using babeljs for ES6, set a dir local variable so you
+;;   can use the nodejs-repl via C-c C-z or M-x nodejs-repl.  The
+;;   project's .dir-locals.el file should be at the top of the
+;;   project and contain something like this:
+;;     ((js2-mode (nodejs-repl-command
+;;                 .
+;;                 "/home/kwbeam/labs/js-lab/node_modules/.bin/babel-node")))
+;;   Where (obviously) you need to change the absolute path.
+;;   TODO:
+;;     * set nodejs-repl-command by finding it in the project's directory tree!
 ;;; Code:
+
+(require 'company)
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
+(add-hook 'js2-mode-hook 'run-dev-hook)
+
+;; Use tern for completions, etc.
 (add-hook 'js2-mode-hook (lambda () (tern-mode t)))
 (add-to-list 'company-backends 'company-tern)
 
-(add-hook 'js2-mode-hook 'run-dev-hook)
-
+;; Setup key mappings for nodejs-repl.
 (add-hook 'js2-mode-hook '(lambda ()
                             (local-set-key "\C-x\C-e" 'nodejs-repl-send-last-sexp)
                             (local-set-key "\C-cr" 'nodejs-repl-send-region)
@@ -29,18 +54,12 @@
                             (local-set-key "\C-cl" 'nodejs-repl-load-file)
                             (local-set-key "\C-c\C-z" 'nodejs-repl-switch-to-repl)))
 
-;; make sure the project's node_modules is always on the path
+;; Make sure the project's node_modules is always on the path (for
+;; eslint, tern, and possibly other things).
 (add-hook 'js2-mode-hook #'add-node-modules-path)
 (add-hook 'projectile-after-switch-project-hook #'add-node-modules-path)
 
 (setq-default js2-basic-offset 2)
-
-;; Cleanup the node and babel repl output
-(require 'comint)
-(add-to-list 'comint-preoutput-filter-functions
-             (lambda (output)
-               (let ((case-fold-search nil))
-                 (replace-regexp-in-string "\033\\[[0-9]+[A-Z]" "" output))))
 
 (provide 'dev-javascript)
 ;;; dev-javascript.el ends here

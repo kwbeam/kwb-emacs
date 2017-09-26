e2169330c4a93972dce7af71efb9f78b12c9d354
Kevin Beam <kwbeam@gmail.com>
2017-09-25 21:25:07 -0600

    Partially rebuilding JS setup. WIP.

--- dev-modes/dev-javascript.el
+++ dev-modes/dev-javascript.el
@@ -16,8 +16,9 @@
 ;;; Code:
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(add-hook 'js2-mode-hook 'run-dev-hook)
 (add-hook 'js2-mode-hook (lambda () (tern-mode t)))
+
+(add-hook 'js2-mode-hook 'run-dev-hook)
 
 (setq-default js2-basic-offset 2)
 
@@ -42,28 +43,28 @@
                (file-executable-p eslint))
       (setq-local flycheck-javascript-eslint-executable eslint))))
 (add-hook 'js2-mode-hook #'kwb/use-eslint-from-node-modules)
-;; See: http://www.cyrusinnovation.com/initial-emacs-setup-for-reactreactnative/
 (add-hook 'projectile-after-switch-project-hook 'kwb/use-eslint-from-node-modules)
 
+;; Use babel repl for es6
 ;; TODO: generify this function and use to set eslint and babel vars
-(defun kwb/use-local-babel-node ()
-  (let* ((root (locate-dominating-file
-                (or (buffer-file-name) default-directory)
-                "node_modules"))
-         (babel-node (and root
-                          (expand-file-name "node_modules/babel-cli/bin/babel-node.js"
-                                            root))))
-    (when (and babel-node
-               (file-executable-p babel-node))
-      (setq babel-repl-cli-program babel-node))))
-(add-hook 'js2-mode-hook #'kwb/use-local-babel-node)
-(add-hook 'projectile-after-switch-project-hook #'kwb/use-local-babel-node)
+;; (defun kwb/use-local-babel-node ()
+;;   (let* ((root (locate-dominating-file
+;;                 (or (buffer-file-name) default-directory)
+;;                 "node_modules"))
+;;          (babel-node (and root
+;;                           (expand-file-name "node_modules/babel-cli/bin/babel-node.js"
+;;                                             root))))
+;;     (when (and babel-node
+;;                (file-executable-p babel-node))
+;;       (setq babel-repl-cli-program babel-node))))
+;; (add-hook 'js2-mode-hook #'kwb/use-local-babel-node)
+;; (add-hook 'projectile-after-switch-project-hook #'kwb/use-local-babel-node)
 
 ;; Cleanup the node and babel repl output
-(require 'comint)
-(add-to-list 'comint-preoutput-filter-functions
-             (lambda (output)
-               (replace-regexp-in-string "\033\\[[0-9]+[A-Z]" "" output)))
+;; (require 'comint)
+;; (add-to-list 'comint-preoutput-filter-functions
+;;              (lambda (output)
+;;                (replace-regexp-in-string "\033\\[[0-9]+[A-Z]" "" output)))
 
 (provide 'dev-javascript)
 ;;; dev-javascript.el ends here

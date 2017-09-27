ad0962563c5d7fe8580ce495efee0893862f2c6f
Kevin Beam <kwbeam@gmail.com>
2017-09-26 22:04:15 -0600

    Fix up lisp mode

--- dev-modes/dev-lisp.el
+++ dev-modes/dev-lisp.el
@@ -1,7 +1,18 @@
+;;; dev-lisp --- Setup emacs for uncommonly-great Common Lisp
+;;; Commentary:
 ;; -----------------
 ;; Lisp
 ;; -----------------
+;; Prerequisite Emacs packages:
+;;   * slime
+;; Prerequisite Language installs:
+;;   * SBCL
+;;; Code:
+
 (setq inferior-lisp-program "sbcl")
+
 (setq slime-contribs '(slime-fancy))
 
 (provide 'dev-lisp)
+
+;;; dev-lisp.el ends here

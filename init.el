b54ab50ccbc1ad74c1802f2fbf4a476d2a14fb26
Kevin Beam <kwbeam@fastmail.com>
2018-07-15 18:45:19 -0600

    Get rid of the dev modes

--- init.el
+++ init.el
@@ -68,6 +68,8 @@
 (setq backup-directory-alist
       `(("." . ,(expand-file-name
                  (concat user-emacs-directory "backups")))))
+(setq custom-file (locate-user-emacs-file ".gnu-emacs-custom"))
+(load custom-file t t)
 (setq browse-url-browser-function 'eww-browse-url)
 
 ;; Use ido mode
@@ -103,17 +105,17 @@
 ;; -------------------------------------
 ;; Define packages
 ;; -------------------------------------
-(use-package exec-path-from-shell
-  :ensure t
-  :pin melpa-stable
-  :config
-  (exec-path-from-shell-initialize))
-
 (use-package color-theme-sanityinc-tomorrow
   :ensure t
   :pin melpa-stable
   :config
   (load-theme 'sanityinc-tomorrow-blue t))
+
+;; (use-package exec-path-from-shell
+;;   :ensure t
+;;   :pin melpa-stable
+;;   :config
+;;   (exec-path-from-shell-initialize))
 
 (use-package multiple-cursors
   :ensure t
@@ -291,23 +293,128 @@
 ;;   (add-to-list 'company-backends 'company-tern))
 
 ;; TypeScript
+;; Prerequisite JS stuff:
+;;   * nvm
+;;   * nvm install --lts
+;;   * mkdir foo && cd foo
+;;   * npm init -y
+;;   * npm install -D @types/node  # workaround for ts-node -- check again
+;;   * npm install -D ts-node
+;;   * npm install -D ts-lint typescript
+;;   * npx tsc --init
+;; Notes:
+;;   When using a repl via ts-comint's M-x run-ts, set a dir local
+;;   variable to point to the correct ts-node executable.  The project's
+;;   .dir-locals.el file should be at the top of the project and
+;;   contain something like this:
+;;     ((typescript-mode (ts-comint-program-command
+;;                        .
+;;                        "/home/kwbeam/labs/ts-lab/node_modules/.bin/ts-node")))
+;;   Where (obviously) you need to change the absolute path.
+;;   TODO:
+;;     * Set the ts-comint-program-command by finding it in the
+;;       project's directory tree!
+(use-package tide
+  :ensure t
+  :pin melpa-stable
+  :after (company flycheck)
+  :mode ("\\.tsx\\'" . web-mode)
+  :config
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  (company-mode +1)
+  (setq company-tooltip-align-annotations t)
+  (setq tide-tsserver-executable "node_modules/.bin/tsserver"))
+
+;; (use-package ts-comint
+;;   :ensure t
+;;   :pin melpa-stable
+;;   :after (tide)
+;;   :defer t
+;;   :bind (("C-x C-e" . 'ts-send-last-sexp)
+;;          ("C-c r" . 'ts-send-region-and-go)
+;;          ("C-c b" . 'ts-send-buffer)
+;;          ("C-M-x" . 'ts-send-last-sexp-and-go)
+;;          ("C-c C-b" . 'ts-send-buffer-and-go)
+;;          ("C-c l" . 'ts-load-file-and-go)))
+
+;; TSX: Use tide & web-mode
+;; (require 'web-mode)
+;; (add-hook 'web-mode-hook 'run-dev-hook)
+;; (add-hook 'web-mode-hook
+;;           (lambda ()
+;;             (when (string-equal "tsx" (file-name-extension buffer-file-name))
+;;               (setup-tide-mode))))
+;; (flycheck-add-mode 'typescript-tslint 'web-mode)
+
 ;; Web
-;;     add-node-modules-path
-;;     exec-path-from-shell
-;;     tern
-;;     company-tern
-;;     js2-mode
-;;     nodejs-repl
-;;     tide
-;;     ts-comint
-;;     web-mode
+;; (require 'web-mode)
+;; (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+;; (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+;; (setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'")))
+
+;; (defun my-web-mode-hook ()
+;;   "Hooks for Web mode."
+;;   (setq web-mode-markup-indent-offset 2)
+;;   (setq web-mode-code-indent-offset 2)
+;;   (setq web-mode-enable-auto-pairing t)
+;;   (setq web-mode-enable-auto-pairing nil)
+;;   )
+;; (add-hook 'web-mode-hook  'my-web-mode-hook)
+;; (add-hook 'web-mode-hook 'run-dev-hook)
+
+;; (defun sp-web-mode-is-code-context (id action context)
+;;   (and (eq action 'insert)
+;;        (not (or (get-text-property (point) 'part-side)
+;;                 (get-text-property (point) 'block-side)))))
+;; (sp-local-pair 'web-mode "<" nil :when '(sp-web-mode-is-code-context))
+;; Use eslint
+;; (flycheck-add-mode 'javascript-eslint 'web-mode)
 
 ;; Python
-;;     company-jedi
-;;     ein
-;;     elpy
-;;     nose
-;;     pipenv
+;; Prerequisite language installs:
+;;   * pyenv
+;;   * pyenv install <python version>
+;;   * pyenv (global|local) <python version>
+;;   * pip install pipenv
+;; Project setup:
+;;   $ pipenv install jedi flake8
+(use-package elpy
+  :ensure t
+  :pin melpa-stable
+  :hook (python-mode . elpy-mode))
 
+;; (defun kwb/python-mode-hook ()
+;;   "Setup all my Python stuff when we enter python mode."
+;;   (pipenv-mode)
+;;   (setq pipenv-projectile-after-switch-function #'pipenv-projectile-after-switch-extended)
+;;   (pyvenv-mode -1)
+;;   (smartparens-mode)
+;;   (add-to-list 'company-backends 'company-jedi)
+;;   (setq python-indent-guess-indent-offset-verbose nil)
+;;   (local-set-key "\C-ca" 'nosetests-all)
+;;   (local-set-key "\C-cm" 'nosetests-module)
+;;   (local-set-key "\C-c." 'nosetests-one)
+;;   (local-set-key "\C-cpa" 'nosetests-pdb-all)
+;;   (local-set-key "\C-cpm" 'nosetests-pdb-module)
+;;   (local-set-key "\C-cp." 'nosetests-pdb-one))
+
+;; (add-hook 'python-mode-hook 'kwb/python-mode-hook)
+
+;; (add-hook 'python-mode-hook 'run-dev-hook)
+
+;; ;; don't use flymake (elpy default), use flycheck
+;; ;; from: https://github.com/jorgenschaefer/elpy/issues/137
+;; (when (require 'flycheck nil t)
+;;   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+;;   (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+;; ;; ein - emacs ipython notebook
+;; (add-hook 'ein:connect-mode-hook 'ein:jedi-setup)
+;; (add-to-list 'company-backends 'ein:company-backend)
+  
 ;; HERE BE DRAGONS
 ;; (add-to-list 'safe-local-variable-values '(lexical-binding . t))

fa6af8daa6d4eaf8d1699303059db00fdf7955ca
Kevin Beam <kwbeam@fastmail.com>
2019-06-01 22:45:39 -0600

    Remove unused langs (tag: all-langs)

--- init.el
+++ init.el
@@ -103,7 +103,7 @@
   :ensure t
   :pin melpa-stable
   :config
-  (load-theme 'sanityinc-tomorrow-night t))
+  (load-theme 'sanityinc-tomorrow-bright t))
 
 (use-package multiple-cursors
   :ensure t
@@ -172,18 +172,6 @@
   :defer t
   :ensure t)
 
-;; Haskell
-(use-package haskell-mode
-  :ensure t
-  :pin melpa-stable
-  :mode ("\\.hs\\'" . haskell-mode))
-
-(use-package intero
-  :ensure t
-  :pin melpa-stable
-  :after (haskell-mode)
-  :hook haskell-mode)
-
 ;; JavaScript
 (use-package js2-mode
   :ensure t
@@ -205,23 +193,6 @@
   :after (js2-mode)
   :hook ((add-node-modules-path . indium-interaction-mode)
          (js2-mode . indium-interaction-mode)))
-
-;; Lisp
-(use-package slime
-  :ensure t
-  :pin melpa-stable
-  :after lisp-mode
-  :defer t
-  :init
-  (setq slime-lisp-implementations
-      '((sbcl ("sbcl"))))
-  (setq slime-contribs '(slime-fancy)))
-
-(use-package slime-company
-  :ensure t
-  :pin melpa-stable
-  :defer t
-  :after (company slime))
 
 ;; Python
 (use-package elpy
@@ -257,15 +228,6 @@
   :config
   (add-hook 'ein:connect-mode-hook 'ein:jedi-setup)
   (add-to-list 'company-backends 'ein:company-backend))
-
-;; Scheme
-(use-package geiser
-  :ensure t
-  :pin melpa-stable
-  :defer t
-  :after (scheme)
-  :init
-  (setq geiser-active-implementations '(racket)))
 
 ;; TypeScript
 (use-package typescript-mode
@@ -316,10 +278,7 @@
 (mapc
  (lambda (hook) (add-hook hook 'kwb-dev-hook))
  '(emacs-lisp-mode-hook
-   haskell-mode-hook
    js2-mode-hook
-   lisp-mode-hook
    python-mode-hook
-   scheme-mode-hook
    typescript-mode-hook
    web-mode-hook))

66296e3b7fe2a219906c534d6c6d4402ddf2914f
Kevin Beam <kbeam@nsidc.org>
2020-10-07 13:19:42 -0600

    Updated / simplified basics

--- modules/basics.el
+++ modules/basics.el
@@ -2,44 +2,39 @@
 ;;; Commentary:
 ;;; Code:
 
-; (require 'server)
-; (if (not (server-running-p))
-;     (server-start))
+(require 'server)
+(if (not (server-running-p))
+    (server-start))
+
 (setq inhibit-startup-message t)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-; (setq visible-bell t)
-; (blink-cursor-mode 1)
-; (setq blink-cursor-blinks 0)
-; (column-number-mode t)
+(blink-cursor-mode 1)
+(setq blink-cursor-blinks 0)
+(column-number-mode t)
+(global-display-line-numbers-mode)
 (setq select-enable-clipboard t)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
-(set-frame-font "Hack-12")
-; (setq uniquify-buffer-name-style 'forward)
-; (when window-system
-;   (setq frame-title-format '(buffer-file-name "%f" ("%b"))))
-; (set-default 'indicate-empty-lines t)
-; (set-default 'indent-tabs-mode nil)
-; (setq tab-width 2)
-(defalias 'yes-or-no-p 'y-or-n-p)
-; (setq echo-keystrokes 0.1)
-(setq create-lockfiles nil)
-(global-auto-revert-mode t)
-(defalias 'auto-revert-tail-mode 'tail-mode)
 (random t)
-(global-set-key (kbd "C-x C-b") 'ibuffer)
-; (add-hook 'text-mode-hook 'turn-on-auto-fill)
-; (add-hook 'text-mode-hook 'turn-on-flyspell)
+
+(set-frame-font "Hack-12")
+(setq uniquify-buffer-name-style 'forward)
+(when window-system
+  (setq frame-title-format '(buffer-file-name "%f" ("%b"))))
+(set-default 'indent-tabs-mode nil)
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(global-auto-revert-mode t)
+(add-hook 'text-mode-hook 'turn-on-auto-fill)
+
+(setq create-lockfiles nil)
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 (setq custom-file (locate-user-emacs-file ".custom.el"))
 (load custom-file t t)
-; (setq browse-url-browser-function 'eww-browse-url)
-
-(global-display-line-numbers-mode)
 
 ;; Use ido mode
 (setq ido-enable-flex-matching t)
@@ -50,33 +45,15 @@
 (ido-mode t)
 
 ;; keybindings
+(global-set-key (kbd "C-x C-b") 'ibuffer)
 (windmove-default-keybindings)
-(define-key global-map (kbd "C-+") 'text-scale-increase)
-(define-key global-map (kbd "C--") 'text-scale-decrease)
-
-;; new line above and below
-;; hat-tip: http://blog.peepcode.com/blog/2012/commanding-your-text-editor/
-(defun newline-previous ()
-  "Insert a blank line above the cursor and move the cursor up one line."
-  (interactive)
-  (beginning-of-line)
-  (newline)
-  (previous-line)
-  (indent-according-to-mode))
-(defun newline-next ()
-  "Insert an indented newline after the current line and move the point to it."
-  (interactive)
-  (end-of-line)
-  (newline-and-indent))
-(define-key global-map (kbd "M-p") 'newline-previous)
-(define-key global-map (kbd "M-n") 'newline-next)
-(global-display-line-numbers-mode)
-(provide 'basics)
 
 (add-hook 'prog-mode-hook
 	  (lambda ()
 	    (set (make-local-variable 'comment-auto-fill-only-comments) t)
 	    (auto-fill-mode t)
 	    (add-hook 'before-save-hook 'delete-trailing-whitespace)))
+
+(provide 'basics)
 
 ;;; basics.el ends here
